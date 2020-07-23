d7476ebd8a890891e3eb122d1f18fe3124876b29
014967 <62296097+014967@users.noreply.github.com>
2020-07-23 15:19:33 +0900

    페이스북 로그인 추가및 로그인시 After_Login으로 intent

--- app/src/main/java/com/example/qrlo/After_Login.java
+++ app/src/main/java/com/example/qrlo/After_Login.java
@@ -3,6 +3,8 @@ package com.example.qrlo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class After_Login extends AppCompatActivity {
 
