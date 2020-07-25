c501d04297968e1e65a28eea9b5397df0cd06e16
ChoWook <41852462+ChoWook@users.noreply.github.com>
2020-07-25 09:03:10 +0900

    CreateQrActivity java 및 xml 파일 생성

---
+++ app/src/main/java/com/example/qrlo/CreateQrActivity.java
@@ -0,0 +1,18 @@
+package com.example.qrlo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateQrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_qr);
+
+        // 내 qr 코드 목록에서 + 버튼 눌렀을 때 나타날 액티비티
+
+        //TODO 주소를 가져올 때 다음 우편번호 주소 API : http://dailyddubby.blogspot.com/2018/02/2-api.html
+    }
+}
