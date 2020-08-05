659e5d659ee768627ed30d829e648aacd37c8202
Shingi <tlsrl6427@naver.com>
2020-08-06 02:32:28 +0900

    .

--- app/src/main/java/com/example/qrlo/After_Login.java
+++ app/src/main/java/com/example/qrlo/After_Login.java
@@ -2,15 +2,29 @@ package com.example.qrlo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 public class After_Login extends AppCompatActivity {
+    Button btn_myQR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after__login);
+
+        btn_myQR = findViewById(R.id.btn_myQR);
+
+        btn_myQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MyQrActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
