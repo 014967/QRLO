f9b02567f3b3d192682bef54cbfc422ae629d35b
014967 <62296097+014967@users.noreply.github.com>
2020-08-13 19:54:46 +0900

    bottom navigation ui 수정

---
+++ app/src/main/java/com/example/qrlo/bottomActivity/Group.java
@@ -0,0 +1,20 @@
+package com.example.qrlo.bottomActivity;
+
+public class Group {
+
+
+    public String when;
+
+    public Group( String when)
+    {
+        this.when= when;
+    }
+
+    public String getWhen() {
+        return when;
+    }
+
+    public void setWhen(String when) {
+        this.when = when;
+    }
+}
